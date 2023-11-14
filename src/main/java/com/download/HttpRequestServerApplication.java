f65fb0b361b68b80c9c38c13f918619043faf303
aliyun5050439044 <ms1878784348@163.com>
2023-11-14 20:21:27 +0800

    初始化SpringBoot工程

---
+++ src/main/java/com/download/HttpRequestServerApplication.java
@@ -0,0 +1,13 @@
+package com.download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpRequestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpRequestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/download/HttpRequestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpRequestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
