6b6b235c15cc7153085a7df20668340da4709096
WYJ <2959380976@qq.com>
2023-11-17 01:25:43 +0800

    files&Settings commit

---
+++ src/main/java/com/download/entity/domain/Files.java
@@ -0,0 +1,40 @@
+package com.download.entity.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class Files {
+
+
+    @TableId(value="file_id", type = IdType.AUTO)
+    private long fileId;
+
+    @TableField("file_name")
+    private String fileName;
+
+    @TableField("file_type")
+    private String filetype;
+
+    @TableField("file_size")
+    private String fileSize;
+
+    /**
+     * 创建时间
+     */
+    @TableField("file_creatTime")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime fileCreatTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField("file_modified")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime fileModifiedTime;
+
+
+}
---
+++ src/main/java/com/download/entity/domain/Settings.java
@@ -0,0 +1,12 @@
+package com.download.entity.domain;
+
+public class Settings {
+
+    private String downloadPath;
+
+    private int maxTasks;
+
+    private long maxDownloadSpeed;
+
+    private long maxUploadSpeed;
+}
