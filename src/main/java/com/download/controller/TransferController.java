d5e1acc1c0a24ef43d4debea2db16ed840e8cc7f
aliyun5050439044 <ms1878784348@163.com>
2023-11-22 22:24:31 +0800

    write Transfer module preliminary function

--- src/main/java/com/download/controller/TransferController.java
+++ src/main/java/com/download/controller/TransferController.java
@@ -3,40 +3,53 @@ package com.download.controller;
 import com.download.aop.LogAnnotation;
 import com.download.entity.ResponseResult;
 import com.download.entity.domain.Transfer;
+import com.download.server.TransferService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/transfer")
 public class TransferController {
 
+    final TransferService transferService;
+
+    @Autowired
+    public TransferController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
     @PostMapping("/submit")
     @LogAnnotation(module = "任务列表模块", operation = "提交任务")
-    public ResponseResult submitTransfer(@RequestBody Transfer transfer) {
-        return ResponseResult.ok("访问提交接口成功");
+    public ResponseResult submitTransfer(@RequestBody List<Transfer> transfers) {
+        return transferService.submitTransfer(transfers);
     }
 
     @PostMapping("/pause")
     @LogAnnotation(module = "任务列表模块", operation = "暂停/启动任务")
-    public ResponseResult pauseTransfer(@RequestParam Long id) {
-        return ResponseResult.ok("访问暂停/启动任务接口成功");
+    public ResponseResult pauseTransfer(@RequestParam List<Long> ids) {
+        return transferService.pauseTransfer(ids);
     }
 
     @PostMapping("/delete")
     @LogAnnotation(module = "任务列表模块", operation = "删除任务")
-    public ResponseResult deleteTransfer(@RequestParam Long id) {
-        return ResponseResult.ok("访问删除任务接口成功");
+    public ResponseResult deleteTransfer(@RequestParam List<Long> ids) {
+        return transferService.deleteTransfer(ids);
+
     }
 
     @PostMapping("/refresh")
     @LogAnnotation(module = "任务列表模块", operation = "重新提交任务")
-    public ResponseResult refreshTransfer(@RequestParam Long id) {
-        return ResponseResult.ok("访问重新提交任务接口成功");
+    public ResponseResult refreshTransfer(@RequestParam List<Long> ids) {
+        return transferService.refreshTransfer(ids);
+
     }
 
-    @PostMapping("/updateThread")
+    @PostMapping("/update_thread")
     @LogAnnotation(module = "任务列表模块", operation = "更改任务下载线程数")
-    public ResponseResult updateThreadTransfer(@RequestParam Long id) {
-        return ResponseResult.ok("访问更改任务下载线程数接口成功");
+    public ResponseResult updateThreadTransfer(@RequestParam Long id, @RequestParam Integer count) {
+        return transferService.updateThreadTransfer(id,count);
     }
 
 }
--- src/main/java/com/download/server/impl/TransferServiceImpl.java
+++ src/main/java/com/download/server/impl/TransferServiceImpl.java
@@ -1,11 +1,38 @@
 package com.download.server.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.download.entity.ResponseResult;
 import com.download.entity.domain.Transfer;
 import com.download.mapper.TransferMapper;
 import com.download.server.TransferService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TransferServiceImpl extends ServiceImpl<TransferMapper, Transfer> implements TransferService {
+    @Override
+    public ResponseResult submitTransfer(List<Transfer> transfers) {
+        return ResponseResult.ok("访问提交接口成功");
+    }
+
+    @Override
+    public ResponseResult pauseTransfer(List<Long> ids) {
+        return ResponseResult.ok("访问暂停/启动任务接口成功");
+    }
+
+    @Override
+    public ResponseResult deleteTransfer(List<Long> ids) {
+        return ResponseResult.ok("访问删除任务接口成功");
+    }
+
+    @Override
+    public ResponseResult refreshTransfer(List<Long> ids) {
+        return ResponseResult.ok("访问重新提交任务接口成功");
+    }
+
+    @Override
+    public ResponseResult updateThreadTransfer(Long id, Integer count) {
+        return ResponseResult.ok("访问更改任务下载线程数接口成功");
+    }
 }
