39d40999c755d53a496fd099784319cab30698f2
aliyun5050439044 <ms1878784348@163.com>
2023-11-27 22:18:32 +0800

    add front file

--- src/main/java/com/download/controller/SettingController.java
+++ src/main/java/com/download/controller/SettingController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-import static org.springframework.http.ResponseEntity.ok;
+
 
 @RestController
 @RequestMapping("/setting")
